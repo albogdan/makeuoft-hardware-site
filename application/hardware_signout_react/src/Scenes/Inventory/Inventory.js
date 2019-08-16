c806c892c0b78120a6f420b97af3ceebff961732
Alex Bogdan <stefanalexb@gmail.com>
2019-08-15 23:21:20 -0400

    Connected api to frontend - fix merge conflicts

--- application/hardware_signout_react/src/Scenes/Inventory/Inventory.js
+++ application/hardware_signout_react/src/Scenes/Inventory/Inventory.js
@@ -3,17 +3,19 @@ import styles from './inventory.module.scss';
 import Component from '../../Components/Inventory/Component/Component';
 import CheckoutHistory from './../../Components/Inventory/CheckoutHistory/CheckoutHistory';
 
+const tags = ["MCUs", "Shields and Breakout Boards", "Sensors", "Computer Peripherals", "Acuators and speakers", "Power Supply", "Passive", "Mechanical", "A - Z", "Z - A"];
+
 export default class Inventory extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { active: false, popup: false, inventory: null}
+    this.state = { active: false, popup: false, popupComponent: null, components: null}
     fetch('http://localhost:8080/api/inventory')
       .then(response => response.json())
       .then(inventory => this.setState({ inventory }));
   }
 
-  // WRITE A SORT FUNCTION
   sort(num) {
+    // WRITE A SORT FUNCTION
     this.setState({ active: num });
   }
 
@@ -25,40 +27,43 @@ export default class Inventory extends PureComponent {
     this.setState({popup: false});
   }
 
+
+  renderPopupComponent = (event) => {
+    this.setState({popupComponent: event})
+  }
+
+
   render() {
-    let { active, popup, inventory } = this.state;
-    console.log("1");
-    console.log(inventory);
+    let { active, popup, popupComponent, components } = this.state;
     const inventoryDataRecevied = (inventory===null);
+    let tagButtons = [];
+    for (let i = 1; i < tags.length+1; i++) {
+        tagButtons.push(<button onClick={() => this.sort(i)} className={active == i ? styles['active'] : null}>{tags[i-1]}</button>);
+    }
+
     return (
       <div className={styles.inventory}>
         {popup &&
-            <CheckoutHistory close={() => {this.closePopup()}}/>
+            <CheckoutHistory event={popupComponent} close={() => {this.closePopup()}}/>
         }
 
         <h1>Inventory</h1>
         <div className={styles.inventoryFilters}>
-          <button onClick={() => this.sort(1)} className={active == 1 ? styles['active'] : null}>MCUs</button>
-          <button onClick={() => this.sort(2)} className={active == 2 ? styles['active'] : null}>Boards</button>
-          <button onClick={() => this.sort(3)} className={active == 3 ? styles['active'] : null}>Sensors</button>
-          <button onClick={() => this.sort(4)} className={active == 4 ? styles['active'] : null}>Actuators</button>
-          <button onClick={() => this.sort(5)} className={active == 5 ? styles['active'] : null}>Peripherals</button>
-          <button onClick={() => this.sort(6)} className={active == 6 ? styles['active'] : null}>Misc Components</button>
-          <button onClick={() => this.sort(7)} className={active == 7 ? styles['active'] : null}>Mechanical Components</button>
-          <button onClick={() => this.sort(8)} className={active == 8 ? styles['active'] : null}>A - Z</button>
-          <button onClick={() => this.sort(9)} className={active == 9 ? styles['active'] : null}>Z - A</button>
+          {tagButtons}
         </div>
 
         <div className={styles.inventoryList}>
+
           {inventoryDataRecevied ? (
             <p>Loading...</p>
           ) : (
-            inventory.map((item, i) => {
+            components.map((item, i) => {
               return (
-                <Component item={item.name}total={item.total} left={item.available} open={ () => {this.openPopup()}} />
+                <Component item={item.name}total={item.total} left={item.available} open={ () => {this.openPopup(); this.renderPopupComponent(item)}} />
               )
             })
           )}
+
 
         </div>
       </div>
