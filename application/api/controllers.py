2db6a3e52981fa750a0cc83025a6f439ef1d28f3
Alex Bogdan <stefanalexb@gmail.com>
2019-08-16 22:40:31 -0400

    Added todo list for requests summary

--- application/api/controllers.py
+++ application/api/controllers.py
@@ -31,3 +31,17 @@ def inventoryAll():
                             "serialnum":part.serial_number, "tags":[tag.tag_name for tag in part.tag_list] })
 
     return jsonify(partsJSON)
+
+
+"""
+
+Requests summary:
+ - JSON of teams with contestants in each team, and whether id was provided for each
+ - JSON of all the parts signed out w/ a time signed out
+ - JSON of all the teams that a part has been signed out to (team, people, quantity to that team, time signed out)
+ - JSON same as api/inventory but only with tag and item name
+ - JSON that returns a list of all the teams and their team names (id + name)
+
+
+
+"""
